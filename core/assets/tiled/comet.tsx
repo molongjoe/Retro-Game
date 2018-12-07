4391d9486f630ab0ae828565e30a91c0a25a8844
Sean McNamara <seanmcnamara97@gmail.com>
2018-12-06 23:54:30 -0500

    Added comets to background

---
+++ core/assets/tiled/comet.tsx
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="comet" tilewidth="320" tileheight="352" tilecount="25" columns="5">
+ <image source="comet.png" width="1600" height="1760"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="10"/>
+   <frame tileid="8" duration="10"/>
+   <frame tileid="9" duration="10"/>
+   <frame tileid="10" duration="10"/>
+   <frame tileid="11" duration="10"/>
+   <frame tileid="12" duration="10"/>
+   <frame tileid="13" duration="10"/>
+   <frame tileid="14" duration="10"/>
+   <frame tileid="15" duration="10"/>
+   <frame tileid="16" duration="10"/>
+   <frame tileid="17" duration="10"/>
+   <frame tileid="18" duration="10"/>
+   <frame tileid="19" duration="10"/>
+   <frame tileid="20" duration="10"/>
+   <frame tileid="21" duration="10"/>
+   <frame tileid="22" duration="9999"/>
+   <frame tileid="22" duration="9999"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="7" duration="9999"/>
+   <frame tileid="8" duration="10"/>
+   <frame tileid="9" duration="10"/>
+   <frame tileid="10" duration="10"/>
+   <frame tileid="11" duration="10"/>
+   <frame tileid="12" duration="10"/>
+   <frame tileid="13" duration="10"/>
+   <frame tileid="14" duration="10"/>
+   <frame tileid="15" duration="10"/>
+   <frame tileid="16" duration="10"/>
+   <frame tileid="17" duration="10"/>
+   <frame tileid="18" duration="10"/>
+   <frame tileid="19" duration="10"/>
+   <frame tileid="20" duration="10"/>
+   <frame tileid="21" duration="10"/>
+   <frame tileid="22" duration="9999"/>
+   <frame tileid="23" duration="9999"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="0" duration="4000"/>
+   <frame tileid="8" duration="10"/>
+   <frame tileid="9" duration="10"/>
+   <frame tileid="10" duration="10"/>
+   <frame tileid="11" duration="10"/>
+   <frame tileid="12" duration="10"/>
+   <frame tileid="13" duration="10"/>
+   <frame tileid="14" duration="10"/>
+   <frame tileid="15" duration="10"/>
+   <frame tileid="16" duration="10"/>
+   <frame tileid="17" duration="10"/>
+   <frame tileid="18" duration="10"/>
+   <frame tileid="19" duration="10"/>
+   <frame tileid="20" duration="10"/>
+   <frame tileid="21" duration="10"/>
+   <frame tileid="22" duration="5000"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="0" duration="6000"/>
+   <frame tileid="8" duration="10"/>
+   <frame tileid="9" duration="10"/>
+   <frame tileid="10" duration="10"/>
+   <frame tileid="11" duration="10"/>
+   <frame tileid="12" duration="10"/>
+   <frame tileid="13" duration="10"/>
+   <frame tileid="14" duration="10"/>
+   <frame tileid="15" duration="10"/>
+   <frame tileid="16" duration="10"/>
+   <frame tileid="17" duration="10"/>
+   <frame tileid="18" duration="10"/>
+   <frame tileid="19" duration="10"/>
+   <frame tileid="20" duration="10"/>
+   <frame tileid="21" duration="10"/>
+   <frame tileid="22" duration="6000"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="0" duration="6000"/>
+   <frame tileid="8" duration="10"/>
+   <frame tileid="9" duration="10"/>
+   <frame tileid="10" duration="10"/>
+   <frame tileid="11" duration="10"/>
+   <frame tileid="12" duration="10"/>
+   <frame tileid="13" duration="10"/>
+   <frame tileid="14" duration="10"/>
+   <frame tileid="15" duration="10"/>
+   <frame tileid="16" duration="10"/>
+   <frame tileid="17" duration="10"/>
+   <frame tileid="18" duration="10"/>
+   <frame tileid="19" duration="10"/>
+   <frame tileid="20" duration="10"/>
+   <frame tileid="21" duration="10"/>
+   <frame tileid="22" duration="7000"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="0" duration="7000"/>
+   <frame tileid="8" duration="10"/>
+   <frame tileid="9" duration="10"/>
+   <frame tileid="10" duration="10"/>
+   <frame tileid="11" duration="10"/>
+   <frame tileid="12" duration="10"/>
+   <frame tileid="13" duration="10"/>
+   <frame tileid="14" duration="10"/>
+   <frame tileid="15" duration="10"/>
+   <frame tileid="16" duration="10"/>
+   <frame tileid="17" duration="10"/>
+   <frame tileid="18" duration="10"/>
+   <frame tileid="19" duration="10"/>
+   <frame tileid="20" duration="10"/>
+   <frame tileid="21" duration="10"/>
+   <frame tileid="22" duration="6300"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="0" duration="1750"/>
+   <frame tileid="8" duration="10"/>
+   <frame tileid="9" duration="10"/>
+   <frame tileid="10" duration="10"/>
+   <frame tileid="11" duration="10"/>
+   <frame tileid="12" duration="10"/>
+   <frame tileid="13" duration="10"/>
+   <frame tileid="14" duration="10"/>
+   <frame tileid="15" duration="10"/>
+   <frame tileid="16" duration="10"/>
+   <frame tileid="17" duration="10"/>
+   <frame tileid="18" duration="10"/>
+   <frame tileid="19" duration="10"/>
+   <frame tileid="20" duration="10"/>
+   <frame tileid="21" duration="10"/>
+   <frame tileid="22" duration="5000"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="8500"/>
+   <frame tileid="8" duration="10"/>
+   <frame tileid="9" duration="10"/>
+   <frame tileid="10" duration="10"/>
+   <frame tileid="11" duration="10"/>
+   <frame tileid="12" duration="10"/>
+   <frame tileid="13" duration="10"/>
+   <frame tileid="14" duration="10"/>
+   <frame tileid="15" duration="10"/>
+   <frame tileid="16" duration="10"/>
+   <frame tileid="17" duration="10"/>
+   <frame tileid="18" duration="10"/>
+   <frame tileid="19" duration="10"/>
+   <frame tileid="20" duration="10"/>
+   <frame tileid="21" duration="10"/>
+   <frame tileid="22" duration="8500"/>
+  </animation>
+ </tile>
+</tileset>
