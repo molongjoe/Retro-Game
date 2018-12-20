2af0f741ec009851e6196dd626c631bd50e42e5e
Sean McNamara <seanmcnamara97@gmail.com>
2018-12-20 17:13:07 -0500

    Added torches to modules

---
+++ core/assets/tiled/Torch.tsx
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Torch" tilewidth="16" tileheight="16" tilecount="9" columns="3">
+ <image source="Torch.png" width="48" height="48"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="1" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="1" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="1" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="3" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="3" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
