50726234b1afc17e5eb5687ace068b04734c850e
Sean McNamara <seanmcnamara97@gmail.com>
2018-12-20 15:30:42 -0500

    Added wood boxes and barrels for design

---
+++ core/assets/tiled/Barrel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Barrel" tilewidth="25" tileheight="32" tilecount="12" columns="4">
+ <image source="Barrel.png" width="100" height="96"/>
+</tileset>
